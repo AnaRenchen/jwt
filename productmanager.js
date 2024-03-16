212d84880c4dc6097770ea77acc29fe80be785b1
AnaRenchen <anamagbh@gmail.com>
2024-03-16 12:19:58 -0300

    primer parte desafio 2

--- productmanager.js
+++ productmanager.js
@@ -1,20 +1,41 @@
+const fs = require("fs");
+
 
 class ProductManager {
 
-    #products
     static idCounter =1;
+   
 
-    constructor (){
-        this.#products=[];
+    constructor (path){
+        this.path= path;
     }
 
-    addProduct(title, description, price, thumbnail, code, stock){
-
-        if (!title || !description || !price || !thumbnail || !code || !stock){
-            return `All fields are required.`;
+    async readProducts(){
+        try{
+        if(fs.existsSync(this.path)){
+            const products = JSON.parse(await fs.promises.readFile(this.path, {encoding: "utf-8"}));
+            return products;
+        }else{
+            return []
         }
+    } catch(error){
+        console.error("Error reading products file:", error.message);
+        return;
+    }
+    }
 
-        const codigorepetido = (this.#products.some(item => item.code == code))
+    async addProduct (title, description, price, thumbnail, code, stock){
+
+        let products = await this.readProducts();
+
+        try{
+            if  (!title || !description || !price || !thumbnail || !code || !stock){
+            console.log("All fields are required.");
+            return;
+        }
+    
+
+        const codigorepetido = products.some(item => item.code == code);
         
         if (codigorepetido){
             return`The code ${code} already exists.`;
@@ -32,25 +53,86 @@ class ProductManager {
         stock
         };
 
-        this.#products.push(newProduct);
+        products.push(newProduct);
+
+        await fs.promises.writeFile(this.path, JSON.stringify(products,null,5));
 
         return `Product "${newProduct.title}" was added.`;
+    } catch (error){
+        console.error("Error adding product.", error.message);
+        return;
+    }
+}
+
+    async getProducts(){
+        try{
+        let displayProducts =  await this.readProducts();
+        return displayProducts;
+        }catch (error){
+            console.error("There was an error reading products file.", error.message);
+            return[];
+        }
     }
 
-    getProducts(){
-        return this.#products;
-    }
-
-    getProductbyId(id){
-
-        const product = this.#products.find(item => item.id == id);
+    async getProductbyId(id){
+        try{
+        let products = await this.readProducts();
+       
+        const product = products.find(item => item.id == id);
         if (product){
-            return product;
+            console.log ("The following roduct found:")
+          return product;
         }else{
-        return `Product with id ${id} was not found!`;
-    }      
+          return `Product with id ${id} was not found!`;
+        }
+     }catch(error){
+        console.error("There was an error searching for product´s id.", error.message);
+        return;
+        
+        }
     }
+
+    async updateProduct(id, objectUpdated){
+        try{
+             let products = await this.readProducts();
+             const productIndex= products.findIndex(item=>item.id===id);
+
+             if(productIndex !== -1){
+                const {id, ...rest}= objectUpdated;
+                products[productIndex]= {...products[productIndex], ...rest};
+                await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5));
+            return `Product with id ${id} was successfully updated.`
+        }else{
+            return `Product with ${id} was not found`;
+        }
+    } catch (error){
+        console.error("There was an error updating the product.", error.message);
+        return;
+    }
+   }      
+    
+
+    async deleteProduct(id){
+        try{
+            let products = await this.readProducts();
+            const filteredProducts=  products.filter(item=>item.id!==id);
+            
+            if (filteredProducts.length !== products.length){
+            await fs.promises.writeFile(this.path, JSON.stringify(filteredProducts, null, 5));
+            return `Product with id ${id} was deleted.`
+             } else{
+                return `Product with id ${id} was not found.`
+            } 
+        }catch(error){
+            console.error("There was an error deleting the product.", error.message);
+            return;
+        }
+
+        }
+
 };
 
 
 module.exports = ProductManager;
+
+    
--- testing1.js
+++ testing1.js
@@ -1,25 +1,48 @@
 const ProductManager = require("./productmanager");
 
+const entorno =async()=>{
 
-const products = new ProductManager();
+const products = new ProductManager("./file/products.json");
 
-console.log (products.getProducts());
 
-console.log (products.addProduct());
+try{
+    
+console.log (await products.getProducts());
 
-console.log(products.addProduct( "producto prueba","este es un producto prueba", 200, "sin imagen", "abc123", 25));
+console.log (await products.addProduct());
 
-console.log (products.getProducts());
+console.log(await products.addProduct("producto prueba","este es un producto prueba", 200, "sin imagen", "abc123", 25));
 
-console.log (products.addProduct("producto prueba2", "este es un producto prueba", 230, "sin imagen", "abc123", 15));
+console.log (await products.getProducts());
 
-console.log (products.getProductbyId(1));
+console.log (await products.addProduct("producto prueba2", "este es un producto prueba", 230, "sin imagen", "abc123", 15));
 
-console.log (products.addProduct( "producto prueba3","este es un producto prueba", 150, "sin imagen", "abc124", 20));
+console.log (await products.getProductbyId(1));
+console.log (await products.getProductbyId(5));
 
-console.log (products.getProducts());
+console.log (await products.addProduct( "producto prueba3","este es un producto prueba", 150, "sin imagen", "abc124", 20));
 
-console.log (products.getProductbyId(2));
+console.log (await products.addProduct( "producto prueba4","este es un producto prueba", 250, "sin imagen", "abc125", 10));
 
-console.log (products.getProductbyId(3));
+console.log (await products.getProducts());
+
+const updateObject ={
+          "id": 2,
+          "title": "producto prueba3 actualizado",
+          "description": "este es un producto prueba3 actualizado",
+          "price": 150,
+}
+
+console.log (await products.updateProduct(2, updateObject));
+
+console.log (await products.deleteProduct(3));
+
+    }catch(error){
+        return (error.message)
+    }
+
+
+}
+
+entorno();
 
