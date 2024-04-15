589319f73959da77bfa0b81c31b33c2754dde002
AnaRenchen <anamagbh@gmail.com>
2024-04-14 22:47:15 -0300

    correcciones

--- src/public/assets/js/script.js
+++ src/public/assets/js/script.js
@@ -4,30 +4,36 @@ let productsUpdate=document.getElementById("products")
 
 socket.on("newproduct", (productsList) => {
     productsUpdate.innerHTML="";
+    productsList.forEach((p) => {
         productsUpdate.innerHTML += `
         <tr>
-          <td>${productsList.title}</td>
-          <td>${productsList.description}</td>
-          <td>${productsList.price}</td>
-          <td>${productsList.code}</td>
-          <td>${productsList.stock}</td>
-          <td>${productsList.status}</td>
-          <td>${productsList.category}</td>
+          <td>${p.title}</td>
+          <td>${p.description}</td>
+          <td>${p.price}</td>
+          <td>${p.code}</td>
+          <td>${p.stock}</td>
+          <td>${p.status}</td>
+          <td>${p.category}</td>
+          <td><img src=${p.thumbnail} alt="{{p.title}}" width="100" /></td>
         </tr>`;
+    });
 });
 
 socket.on("deletedproduct", (products) => {
     productsUpdate.innerHTML="";
+    products.forEach((p) => {
         productsUpdate.innerHTML += `
         <tr>
-          <td>${products.title}</td>
-          <td>${products.description}</td>
-          <td>${products.price}</td>
-          <td>${products.code}</td>
-          <td>${products.stock}</td>
-          <td>${products.status}</td>
-          <td>${products.category}</td>
+          <td>${p.title}</td>
+          <td>${p.description}</td>
+          <td>${p.price}</td>
+          <td>${p.code}</td>
+          <td>${p.stock}</td>
+          <td>${p.status}</td>
+          <td>${p.category}</td>
+          <td><img src="${p.thumbnail}" alt="{{p.title}}" width="100" /></td>
         </tr>`;
+    });
 });
 
 
